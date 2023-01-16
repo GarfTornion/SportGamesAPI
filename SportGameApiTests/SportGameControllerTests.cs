d2794248ef823a370df777f7869154a12983a38e
GarfTornion <billy.ward@hotmail.com>
2023-01-16 21:06:25 +0200

    add test project and write initial tests

---
+++ SportGameApiTests/SportGameControllerTests.cs
@@ -0,0 +1,188 @@
+﻿using AutoFixture;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using SportGamesAPI.Controllers;
+using SportGamesAPI.Data.Interfaces;
+using SportGamesAPI.Hubs;
+using SportGamesAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportGameApiTests
+{
+    [TestClass]
+    public class SportGameControllerTests
+    {
+        private Mock<ISportGameRepository> _sportGameRepository;
+        private IHubContext<SportGameHub> _hub;
+        private Fixture _fixture;
+        private SportGameController _controller;
+
+        public SportGameControllerTests()
+        {
+            _sportGameRepository = new Mock<ISportGameRepository>();
+            //_hub = new IHubContext<SportGameHub>();
+            _fixture = new Fixture();
+            _controller = new SportGameController(_hub, _sportGameRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task GetSportGames_ReturnsSportGamesList()
+        {
+            // Arrange
+            var sportGames = _fixture.CreateMany<SportGame>(20).ToList();
+            _sportGameRepository.Setup(x => x.GetSportGames()).ReturnsAsync(sportGames);
+
+            // Act
+            var result = await _controller.GetSportGames();
+
+            // Assert
+            Assert.IsInstanceOfType(result.Value, typeof(List<SportGame>));
+        }
+
+        [TestMethod]
+        public async Task GetSportGames_ThrowInternalError()
+        {
+            // Arrange
+            _sportGameRepository.Setup(x => x.GetSportGames()).ThrowsAsync(new Exception());
+
+            // Act
+            var result = await _controller.GetSportGames();
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(StatusCodeResult));
+            Assert.AreEqual(500, (result.Result as StatusCodeResult).StatusCode);
+        }
+
+        [TestMethod]
+        public async Task GetSportGame_ReturnsSportGame()
+        {
+            // Arrange
+            var sportGame = _fixture.Create<SportGame>();
+            _sportGameRepository.Setup(x => x.GetSportGame(It.IsAny<int>())).ReturnsAsync(sportGame);
+
+            // Act
+            var result = await _controller.GetSportGame(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result.Value, typeof(SportGame));
+        }
+
+        [TestMethod]
+        public async Task GetSportGame_ThrowInternalError()
+        {
+            // Arrange
+            _sportGameRepository.Setup(x => x.GetSportGame(It.IsAny<int>())).ThrowsAsync(new Exception());
+
+            // Act
+            var result = await _controller.GetSportGame(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(StatusCodeResult));
+            Assert.AreEqual(500, (result.Result as StatusCodeResult).StatusCode);
+        }
+
+        [TestMethod]
+        public async Task CreateSportGame_ReturnsSportGame()
+        {
+            // Arrange
+            var sportGame = _fixture.Create<SportGame>();
+            _sportGameRepository.Setup(x => x.CreateSportGame(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(sportGame);
+
+            var sportGames = _fixture.CreateMany<SportGame>(20).ToList();
+            _sportGameRepository.Setup(x => x.GetSportGames()).ReturnsAsync(sportGames);
+
+            //_hub.Setup(x => x.SendUpdatedGames(It.IsAny<List<SportGame>>()));
+
+
+            // Act
+            var result = await _controller.CreateSportGame("team1", "team2");
+
+            // Assert
+            Assert.IsInstanceOfType(result.Value, typeof(SportGame));
+        }
+
+        [TestMethod]
+        public async Task CreateSportGame_ThrowInternalError()
+        {
+            // Arrange
+            _sportGameRepository.Setup(x => x.CreateSportGame(It.IsAny<string>(), It.IsAny<string>())).ThrowsAsync(new Exception());
+
+            // Act
+            var result = await _controller.CreateSportGame("team1", "team2");
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(StatusCodeResult));
+            Assert.AreEqual(500, (result.Result as StatusCodeResult).StatusCode);
+        }
+
+        [TestMethod]
+        public async Task UpdateSportGame_ReturnsSportGame()
+        {
+            // Arrange
+            var sportGame = _fixture.Create<SportGame>();
+            _sportGameRepository.Setup(x => x.UpdateSportGame(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(sportGame);
+
+            var sportGames = _fixture.CreateMany<SportGame>(20).ToList();
+            _sportGameRepository.Setup(x => x.GetSportGames()).ReturnsAsync(sportGames);
+
+            //_hub.Setup(x => x.SendUpdatedGames(It.IsAny<List<SportGame>>()));
+
+            // Act
+            var result = await _controller.UpdateSportGame(1, 1, 1);
+
+            // Assert
+            Assert.IsInstanceOfType(result.Value, typeof(SportGame));
+        }
+
+        [TestMethod]
+        public async Task UpdateSportGame_ThrowInternalError()
+        {
+            // Arrange
+            _sportGameRepository.Setup(x => x.UpdateSportGame(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>())).ThrowsAsync(new Exception());
+
+            // Act
+            var result = await _controller.UpdateSportGame(1, 1, 1);
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(StatusCodeResult));
+            Assert.AreEqual(500, (result.Result as StatusCodeResult).StatusCode);
+        }
+
+        [TestMethod]
+        public async Task FinishSportGame_ReturnSportGame()
+        {
+            // Arrange
+            var sportGame = _fixture.Create<SportGame>();
+            _sportGameRepository.Setup(x => x.FinishSportGame(It.IsAny<int>())).ReturnsAsync(sportGame);
+
+            var sportGames = _fixture.CreateMany<SportGame>(20).ToList();
+            _sportGameRepository.Setup(x => x.GetSportGames()).ReturnsAsync(sportGames);
+
+            // Act
+            var result = await _controller.FinishSportGame(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result.Value, typeof(SportGame));
+        }
+
+        [TestMethod]
+        public async Task FinishSportGame_ThrowInternalError()
+        {
+            // Arrange
+            _sportGameRepository.Setup(x => x.FinishSportGame(It.IsAny<int>())).ThrowsAsync(new Exception());
+
+            // Act
+            var result = await _controller.FinishSportGame(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(StatusCodeResult));
+            Assert.AreEqual(500, (result.Result as StatusCodeResult).StatusCode);
+
+        }
+    }
+}
--- SportGamesAPI/Controllers/SportGameController.cs
+++ SportGamesAPI/Controllers/SportGameController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using SportGamesAPI.Data.Interfaces;
 using SportGamesAPI.Hubs;
-//debug
 using System.Diagnostics;
 
 namespace SportGamesAPI.Controllers
@@ -24,40 +23,83 @@ namespace SportGamesAPI.Controllers
         [HttpGet("GetSportGames")]
         public async Task<ActionResult<List<SportGame>>> GetSportGames()
         {
-            var result = await _sportGameRepository.GetSportGames();
-            return Ok(result);
+            try
+            {
+                var result = await _sportGameRepository.GetSportGames();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpGet("GetSportGame/{id}")]
         public async Task<ActionResult<SportGame>> GetSportGame(int id)
         {
-            var result = await _sportGameRepository.GetSportGame(id);
-            return Ok(result);
+            try
+            {
+                var result = await _sportGameRepository.GetSportGame(id);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpPost("CreateSportGame")]
         public async Task<ActionResult<SportGame>> CreateSportGame(string team1Name, string team2Name)
         {
-            var result = await _sportGameRepository.CreateSportGame(team1Name, team2Name);
-            _hub.Clients.All.SendAsync("updategames", await GetSportGames());
-            return Ok(result);
+            try
+            {
+                var result = await _sportGameRepository.CreateSportGame(team1Name, team2Name);
+                var games = await GetSportGames();
+                await _hub.Clients.All.SendAsync("updategames", games.Value);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpPut("UpdateSportGame")]
         public async Task<ActionResult<SportGame>> UpdateSportGame(int id, int team1Score, int team2Score)
         {
-            var result = await _sportGameRepository.UpdateSportGame(id, team1Score, team2Score);
-            Debug.WriteLine(GetSportGames().Result);
-            _hub.Clients.All.SendAsync("updategames", await GetSportGames());
-            return Ok(result);
+            try
+            {
+                var result = await _sportGameRepository.UpdateSportGame(id, team1Score, team2Score);
+                var games = await GetSportGames();
+                await _hub.Clients.All.SendAsync("updategames", games.Value);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpPut("FinishSportGame")]
         public async Task<ActionResult<SportGame>> FinishSportGame(int id)
         {
-            var result = await _sportGameRepository.FinishSportGame(id);
-            _hub.Clients.All.SendAsync("updategames", await GetSportGames());
-            return Ok(result);
+            try
+            {
+                var result = await _sportGameRepository.FinishSportGame(id);
+                var games = await GetSportGames();
+                _hub.Clients.All.SendAsync("updategames", games.Value);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
         }
 
     }
